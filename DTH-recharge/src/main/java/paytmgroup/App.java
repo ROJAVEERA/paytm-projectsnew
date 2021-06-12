15b4603ae95022c74972f85d7e32b6a5cb92a7b7
roja <krojakumari99@gmail.com>
2021-06-12 11:34:21 +0530

    developer develops code on DTH-recharge

---
+++ DTH-recharge/src/main/java/paytmgroup/App.java
@@ -0,0 +1,13 @@
+package paytmgroup;
+
+/**
+ * DTH-recharge
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi DTH-recharge in paytm" );
+    }
+}
