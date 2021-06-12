ffc19951b91e9350d6df30ebffa74eb9c27c9e44
roja <krojakumari99@gmail.com>
2021-06-12 11:31:17 +0530

    developer develops code on bus ticket

---
+++ bus-ticket/src/main/java/paytmgroup/App.java
@@ -0,0 +1,13 @@
+package paytmgroup;
+
+/**
+ * bus-tickets
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi this is bus tickets in paytm" );
+    }
+}
